819f9bbb16f3f12221629da1e11864bf895b86bf
godoimatheus <matheus.godoi@live.com>
2023-08-21 22:13:24 -0300

    refactor: altera nome do arquivo

---
+++ src/scraper.py
@@ -0,0 +1,136 @@
+import re
+import os
+from datetime import datetime
+from bs4 import BeautifulSoup
+import requests
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from pymongo.errors import DuplicateKeyError
+
+uri = os.environ.get("MONGODB_URI", "mongodb://localhost:27017")
+client = MongoClient(uri, server_api=ServerApi("1"))
+try:
+    client.admin.command("ping")
+    print("Conectando ao banco de dados")
+except Exception as e:
+    print(e)
+db = client["issues"]
+collection = db["vagas"]
+db["vagas"].create_index([("issue_number", pymongo.ASCENDING)], unique=True)
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+}
+
+URL = "https://github.com/backend-br/vagas/issues?q=is%3Aissue+email+OR+mail+in%3Abody+label%3APython"
+site = requests.get(URL, headers=headers, timeout=10)
+soup = BeautifulSoup(site.content, "html.parser")
+open_issues = (
+    soup.find("div", class_="flex-auto d-none d-lg-block no-wrap")
+    .text.strip()
+    .split(" ")[0]
+)
+closed_issues = (
+    soup.find("div", class_="flex-auto d-none d-lg-block no-wrap")
+    .text.strip()
+    .split(" ")[-2]
+)
+total_issues = int(open_issues) + int(closed_issues)
+print(f"total issues: {total_issues}")
+if total_issues > 25:
+    last_page = soup.find("div", class_="pagination").text.strip().split(" ")[-2]
+    print(f"Total pages{last_page}")
+
+for page in range(1, int(last_page) + 1):
+    print(f"Page: {page}")
+    URL = f"https://github.com/backend-br/vagas/issues?page={page}&q=is%3Aissue+email+OR+mail+in%3Abody+label%3APython"
+    site = requests.get(URL, headers=headers, timeout=10)
+    soup = BeautifulSoup(site.content, "html.parser")
+    jobs = soup.find_all(
+        "div",
+        class_="Box-row Box-row--focus-gray p-0 mt-0 js-navigation-item js-issue-row",
+    )
+    print(len(jobs))
+    for job in jobs:
+        issue_number = (
+            job.find("span", class_="opened-by").get_text().strip().split("\n")[0]
+        )
+        print(issue_number)
+
+        status = job.find("span", class_="tooltipped tooltipped-e").get("aria-label")
+        print(status)
+
+        title = job.find(
+            class_="Link--primary v-align-middle no-underline h4 js-navigation-open markdown-title"
+        ).get_text()
+        print(title)
+
+        opened = job.find("relative-time").get("datetime")
+        issue_date = datetime.strptime(opened, "%Y-%m-%dT%H:%M:%SZ")
+        print(issue_date)
+
+        labels = job.find_all("a", class_="IssueLabel hx_IssueLabel")
+        labels_list = []
+        for label in labels:
+            labels_list.append(label.get_text().strip())
+        print(labels_list)
+
+        author = job.find("a", class_="Link--muted").get_text()
+        print(author)
+
+        URL_GITHUB = "https://github.com"
+
+        url_author = URL_GITHUB + "/" + author
+        print(url_author)
+
+        link_job = job.find(
+            "a",
+            class_="Link--primary v-align-middle no-underline h4 js-navigation-open markdown-title",
+        ).get("href")
+        url_job = URL_GITHUB + link_job
+        print(url_job)
+        site = requests.get(url_job, headers=headers, timeout=10)
+        soup = BeautifulSoup(site.content, "html.parser")
+        job_detail = soup.find("div", class_="edit-comment-hide")
+        try:
+            EMAIL = job_detail.find(href=re.compile("mailto")).get_text()
+            print(EMAIL)
+        except AttributeError:
+            EMAIL = "not found"
+            print(EMAIL)
+        search_time = datetime.utcnow()
+        try:
+            db["vagas"].insert_one(
+                {
+                    "issue_number": issue_number,
+                    "status": status,
+                    "title": title,
+                    "last_update": issue_date,
+                    "labels": labels_list,
+                    "author": author,
+                    "author_page": url_author,
+                    "email": EMAIL,
+                    "url_issue": url_job,
+                    "search_time": search_time,
+                }
+            )
+        except DuplicateKeyError:
+            db["vagas"].update_one(
+                {"issue_number": issue_number},
+                {
+                    "$set": {
+                        "status": status,
+                        "title": title,
+                        "last_update": issue_date,
+                        "labels": labels_list,
+                        "author": author,
+                        "author_page": url_author,
+                        "email": EMAIL,
+                        "url_issue": url_job,
+                        "search_time": search_time,
+                    }
+                },
+            )
+        print()
