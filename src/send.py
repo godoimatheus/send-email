8513f205cce1319077e4c8e371a07958a117ea23
godoimatheus <matheus.godoi@live.com>
2023-08-30 09:44:27 -0300

    feat: permite apenas emails nao usados e atualiza status

--- src/send.py
+++ src/send.py
@@ -1,9 +1,13 @@
 import smtplib
-import email.message
 import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# from email.mime.application import MIMEApplication
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from user import MY_EMAIL, MY_PASSWORD
+from settings import MY_EMAIL, MY_PASSWORD
+from settings import MY_SUBJECT, MY_BODY
 
 uri = os.environ.get("MONGODB_URI", "mongodb://localhost:27017")
 client = MongoClient(uri, server_api=ServerApi("1"))
@@ -17,22 +21,38 @@ collection = db["vagas"]
 
 
 def send_email():
-    email_list = collection.distinct("email")
-    for email_issues in email_list:
-        body_email = "áéíóú"
-        msg = email.message.Message()
-        msg["Subject"] = "Assunto"
+    email_list = collection.distinct("email", {"send": False})
+    for email in email_list[:50]:
+        if email == "not found":
+            continue
+
+        msg = MIMEMultipart()
         msg["From"] = MY_EMAIL
-        msg["To"] = email_issues
         password = MY_PASSWORD
-        msg.add_header("Content-Type", "text/html; charset=utf-8")
-        msg.set_payload(body_email, "utf-8")
+        msg["To"] = email
+        msg["Subject"] = MY_SUBJECT
+        body = MY_BODY
+        msg.attach(MIMEText(body, "html"))
+
+        # pdf_path = "cv/curriculum.pdf"
+        # with open(pdf_path, "rb") as pdf:
+        #     pdf_att = MIMEApplication(pdf.read(), _subtype="pdf")
+        #     pdf_att.add_header(
+        #         "content-disposition", "attachment; filename=curriculum.pdf"
+        #     )
+        #     msg.attach(pdf_att)
+
         smtp = smtplib.SMTP("smtp.gmail.com: 587")
         smtp.starttls()
         smtp.login(msg["From"], password)
         smtp.sendmail([msg["From"]], [msg["To"]], msg.as_string().encode("utf-8"))
-        print(f"Sending email to: {email_issues}")
+        smtp.quit()
+        print(f"Sending email to: {email}")
+
+        collection.update_many({"email": email}, {"$set": {"send": True}})
+
     print("Finished sending the emails")
 
 
-# send_email()
+if __name__ == "__main__":
+    send_email()
