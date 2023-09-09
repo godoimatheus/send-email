ffb70a3dc9d62a689d563e66b9df613846289072
godoimatheus <matheus.godoi@live.com>
2023-09-09 17:49:16 -0300

    test: verifica se conexao esta com erro

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,13 +1,31 @@
 import unittest
 from io import StringIO
 from unittest.mock import patch
-from src.scraper import connect_to_database
+from pymongo.collection import Collection
+from pymongo.errors import PyMongoError
+from src import scraper
 
 
 class TestDatabase(unittest.TestCase):
     @patch("sys.stdout", new_callable=StringIO)
     def test_connect_to_database(self, mock_stdout):
-        self.assertIsNotNone(connect_to_database())
+        result = scraper.connect_to_database()
+        self.assertIsNotNone(result)
         self.assertIn(
             "Successfully connected to the database...", mock_stdout.getvalue()
         )
+
+        self.assertIsInstance(result, Collection)
+        indexes = result.index_information()
+        self.assertTrue("issue_number_1" in indexes)
+
+    @patch("src.scraper.MongoClient")
+    def test_connection_error_database(self, mock_mongo_client):
+        mock_instance = mock_mongo_client.return_value
+        mock_instance.admin.command.side_effect = PyMongoError("Error")
+        result = scraper.connect_to_database()
+        self.assertIsNone(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
