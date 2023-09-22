430d687401d13e43ef2393d384431e7757d851b9
godoimatheus <matheus.godoi@live.com>
2023-09-22 12:33:31 -0300

    test: adiciona teste de conexao com banco de dados

--- tests/test_send.py
+++ tests/test_send.py
@@ -1,13 +1,27 @@
 import unittest
 from unittest.mock import patch
 from io import StringIO
-from src.send import connect_to_database
+from pymongo.collection import Collection
+from pymongo.errors import PyMongoError
+from src import send
 
 
-class TestDatabase(unittest.TestCase):
+class TestDatabaseConnection(unittest.TestCase):
     @patch("sys.stdout", new_callable=StringIO)
     def test_connect_to_database(self, mock_stdout):
-        self.assertIsNotNone(connect_to_database())
+        result = send.connect_to_database()
+        self.assertIsNotNone(result)
         self.assertIn(
             "Successfully connected to the database...", mock_stdout.getvalue()
         )
+
+        self.assertIsInstance(result, Collection)
+        indexes = result.index_information()
+        self.assertTrue("issue_number_1" in indexes)
+
+    @patch("src.send.MongoClient")
+    def test_connection_error_database(self, mock_mongo_client):
+        mock_instance = mock_mongo_client.return_value
+        mock_instance.admin.command.side_effect = PyMongoError("Error")
+        result = send.connect_to_database()
+        self.assertIsNone(result)
